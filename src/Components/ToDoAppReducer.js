84e6e9d35c61e35d61a4d71062a2be28f500679b
Trung Nguyen Hoang <trungnguyenhoang2k@gmail.com>
2022-01-22 01:33:23 +0700

    abc

---
+++ src/Components/ToDoAppReducer.js
@@ -0,0 +1,26 @@
+import { useReducer, useRef } from "react";
+
+const initState = {
+    inputTask: '',
+    tasks: []
+}
+
+const SET_INPUT_TASK = 'set_input_task';
+const ADD_TASK = 'add_task';
+const DELETE_TASK = 'delete_task';
+
+const 
+
+const ToDoAppReducer = () => {
+
+
+    return (
+        <div className="ToDoAppReducer">
+            <ul className="Tasks list">
+
+            </ul>
+        </div>
+    )
+}
+
+export default ToDoAppReducer;
